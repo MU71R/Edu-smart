baed8f1d8d63c6da50eee853bad2a4dc5b271f51
MU71R <mastafasamay351@gmail.com>
2025-09-12 18:47:18 +0300

    update

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -8,8 +8,8 @@ const upload = require("../controllers/cloudinary");
 router.post('/create', verifyTokenMiddleware, upload.single("coverImage"), courseController.createCourse);  
 router.get('/get-package/:id', courseController.getPackageById); 
 router.get('/get-all-packages', verifyTokenMiddleware, courseController.getallpackages);              // GET /api/courses -> لجلب كل الكورسات
-router.get('/all', verifyTokenMiddleware, courseController.getAllCourses);                         // GET /api/courses -> لجلب كل الكورسات
-router.get('/get/:id', verifyTokenMiddleware, courseController.getCourseById);          // GET /api/courses/:id -> لجلب كورس واحد بالـ ID
+router.get('/all',courseController.getAllCourses);                         // GET /api/courses -> لجلب كل الكورسات
+router.get('/get/:id', courseController.getCourseById);          // GET /api/courses/:id -> لجلب كورس واحد بالـ ID
 router.get('/my-courses', verifyTokenMiddleware, courseController.getMyCourses); // GET /api/courses/my-courses -> لجلب كورسات المدرب
 router.put('/update/:id', verifyTokenMiddleware, courseController.updateCourse);           // PUT /api/courses/:id -> لتحديث كورس بالـ ID
 router.delete('/delete/:id', verifyTokenMiddleware, courseController.deleteCourse);        // DELETE /api/courses/:id -> لحذف كورس بالـ ID
